038a8bc4641d4a0723231ab683a27eebd4cf49b1
user <897100198@qq.com>
2018-07-06 18:26:36 +0800

    修改连接数据库文件

--- models/conn.js
+++ models/conn.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/expressStudy', {useMongoClient: true});
+mongoose.connect('mongodb://localhost:27017/expressStudy');
 
 module.exports = mongoose;
