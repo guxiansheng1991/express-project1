584ed6b680acbac25a02d4add00ef8bac2cafa19
user <897100198@qq.com>
2018-07-06 18:19:47 +0800

    用户模块完成

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,37 @@ router.get('/list', function(req, res, next) {
       title: '用户列表',
       list: data
     };
-    res.render('users', content);
+    res.render('user/users', content);
+  });
+});
+router.get('/add', function(req, res, next) {
+  let content = {
+    title: '新增用户',
+    status: ''
+  };
+  res.render('user/add', content);
+});
+router.post('/addAction', function(req, res) {
+  let newUser = new userModel({
+    username: req.body.username,
+    userGender: req.body.userGender,
+    userAge: req.body.userAge
+  });
+  newUser.save(function(err, data) {
+    if(err) res.render('error', util.handleError(err));
+    let content = {
+      title: '新增用户',
+      status: '新增用户成功'
+    };
+    res.render('user/add', content);
+  });
+});
+
+router.post('/delete', function(req, res) {
+  let _id = req.body._id;
+  userModel.remove({_id: _id}, function(err, data) {
+    if(err) res.render('error', util.handleError(err));
+    res.status(200).json({msg: '删除成功'});
   });
 });
 
